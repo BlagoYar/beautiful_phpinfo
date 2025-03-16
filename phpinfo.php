180eece1eb683a8ca2249ca5bc5d35382aafd86e
БлагоЯр <3672314+BlagoYar@users.noreply.github.com>
2025-03-17 00:15:05 +0200

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -51,7 +51,7 @@
         }
         return false;
     }
-    $cssFile = isMobile() ? '/css/php_smart.css' : '/css/php_out.css';
+    $cssFile = isMobile() ? '/res/css/php_smart.css' : '/res/css/php_out.css';
 
     # Перехват вывода для очистки стилей и "шапки" HTML по умолчанию
     ob_start();
@@ -68,14 +68,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>phpinfo by BlagoYar | PHP v<?php echo $phpVersion; ?></title>
     <meta http-equiv="Cache-Control" name="ROBOTS" content="NOINDEX,NOFOLLOW,NOARCHIVE,NOIMAGEINDEX,NO-CACHE,NOSNIPPET" />
+    <title>phpinfo by BlagoYar | PHP v<?php echo $phpVersion; ?></title>
     <?php if (isMobile()): ?>
         <link rel="stylesheet" href="<?php echo $cssFile; ?>" type="text/css">
     <?php endif; ?>
-    <link rel="stylesheet" href="/css/php_out.css" type="text/css">
-    <link rel="shortcut icon" href="/imgs/php_out_favicon.png" type="image/png">
-    <script defer src="/js/php_out.js"></script>
+    <link rel="stylesheet" href="/res/css/php_out.css" type="text/css">
+    <link rel="shortcut icon" href="/res/imgs/php_out_favicon.png" type="image/png">
+    <script defer src="/res/js/php_out.js"></script>
 </head>
 <body>
     <?php
@@ -97,12 +97,12 @@
             '/<td class="v">available <\/td>/' => '<td class="v"><span class="v available-text">available</span></td>',
             '/<td class="v">disabled <\/td>/' => '<td class="v"><span class="v disabled-text">disabled</span></td>',
             '/<td class="v">active <\/td>/' => '<td class="v"><span class="v enabled-text">active</span></td>',
-            '/<td class="v">1<\/td>/' => '<td class="v"><span class="v enabled-text">1</span></td>',
+            '/<td class="v">1<\/td>/' => '<td class="v"><span class="v enabled-int">1</span></td>',
             '/<td class="v">enabled <\/td>/' => '<td class="v"><span class="v enabled-text">enabled</span></td>',
             '/<td class="v">Enabled <\/td>/' => '<td class="v"><span class="v enabled-text">Enabled</span></td>',
-            '/<td class="v">On<\/td>/' => '<td class="v enabled-text">On</td>',
-            '/<td class="v">Off<\/td>/' => '<td class="v disabled-text">Off</td>',
-            '/<td class="v">0<\/td>/' => '<td class="v"><span class="v disabled-text">0</span></td>',
+            '/<td class="v">On<\/td>/' => '<td class="v"><span class="v enabled-on">On</span></td>',
+            '/<td class="v">Off<\/td>/' => '<td class="v"><span class="v disabled-off">Off</span></td>',
+            '/<td class="v">0<\/td>/' => '<td class="v"><span class="v disabled-int">0</span></td>',
             '/<td class="v">disabled <\/td>/' => '<td class="v"><span class="v disabled-text">disabled</span></td>',
             '/<td class="v">Disabled <\/td>/' => '<td class="v"><span class="v disabled-text">Disabled</span></td>',
             '/<td class="v">disabled \(install ext\/bz2\) <\/td>/' => '<td class="v"><span class="v disabled-text">disabled</span> (install ext/bz2)</td>',
@@ -111,17 +111,22 @@
             '/<td class="v">nocache<\/td>/' => '<td class="v"><span class="v no-text">nocache</span></td>',
             '/<td class="v">none<\/td>/' => '<td class="v"><span class="v no-text">none</span></td>',
             '/<td class="v"><i>no value<\/i><\/td>/' => '<td class="v"><i class="no-text">no value</i></td>',
-            '/<tr class="h"><th>(.*?)<\/th><th>enabled<\/th><\/tr>/' => '<tr class="h"><th>$1</th><th class="enabled-text">enabled</th></tr>',
-            '/<td class="v">on<\/td>/' => '<td class="v enabled-text">on</td>',
-            '/<td class="v">off<\/td>/' => '<td class="v disabled-text">off</td>',
-            '/<tr class="h"><th>(.*?)<\/th><th>disabled<\/th><\/tr>/' => '<tr class="h"><th>$1</th><th class="disabled-text">disabled</th></tr>',
-            '/<td class="v">OK <\/td>/' => '<td class="v enabled-text">OK </td>',
+            '/<tr class="h"><th>(.*?)<\/th><th>enabled<\/th><\/tr>/' => '<tr class="h"><th>$1</th><th class="enabled-mod">enabled</th></tr>',
+            '/<td class="v">on<\/td>/' => '<td class="v"><span class="v enabled-text">on</span></td>',
+            '/<td class="v">off<\/td>/' => '<td class="v"><span class="v enabled-text">off</span></td>',
+            '/<tr class="h"><th>(.*?)<\/th><th>disabled<\/th><\/tr>/' => '<tr class="h"><th>$1</th><th class="disabled-mod">disabled</th></tr>',
+            '/<td class="v">OK <\/td>/' => '<td class="v"><span class="v enabled-ok">OK </span></td>',
             '/<td class="v">no-cache<\/td>/' => '<td class="v"><span class="v no-text">no-cache</span></td>',
-            '/<td class="v">0 <\/td>/' => '<td class="v"><span class="v disabled-text">0 </span></td>',
+            '/<td class="v">0 <\/td>/' => '<td class="v"><span class="v disabled-int">0 </span></td>',
             '/<td class="v"><i>no value<\/i> <\/td>/' => '<td class="v"><i class="no-text">no value</i> </td>',
         ];
 
         $phpinfo = preg_replace(array_keys($replacements), array_values($replacements), $phpinfo);
+
+        $insertHtml = '<div id="logo-text">
+            <a href="https://github.com/BlagoYar/beautiful_phpinfo" class="logo-text" target="_blank" rel="noopener noreferrer">Designed by BlagoYar</a>
+        </div>';
+        $phpinfo .= "\n" . $insertHtml; // Добавляем в конец phpinfo
 
         echo $phpinfo;
 
